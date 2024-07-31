d449d2e617ab2469e557e4f398bc508bd005b1ab
Francisco <francisco63554@hotmail.com>
2024-07-31 11:22:57 -0500

    conexion con bd local, agregar al carrito, calculo de total, eliminar del carrito

--- src/components/Guitar.jsx
+++ src/components/Guitar.jsx
@@ -13,9 +13,9 @@ export default function Guitar({guitar, addToCart}) {
                 <p>{description}</p>
                 <p className="fw-black text-primary fs-3">${price}</p>
                 <button
-                    onClick={() => addToCart(guitar)}
                     type="button"
                     className="btn btn-dark w-100"
+                    onClick={() => addToCart(guitar)}
                 >Agregar al Carrito</button>
             </div>
         </div>
