b7478be33b909a5bad5888f7d14c144c54efbbb2
Francisco <francisco63554@hotmail.com>
2024-07-31 12:49:10 -0500

    seteado el local storage para mantener info cuando se actualiza la pag

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,20 @@ import { db } from './data/db'
 
 function App() {
 
+  const initialCart = () => {
+    const localStorageCart = localStorage.getItem('cart')
+    return localStorageCart ? JSON.parse(localStorageCart) : []
+  }
+
   const [data, setData] = useState(db)
-  const [cart, setCart] = useState([])
+  const [cart, setCart] = useState(initialCart)
 
   const MAX_ITEMS = 5
   const MIN_ITEMS = 1
 
+  useEffect(() => {
+    localStorage.setItem('cart', JSON.stringify(cart))
+  }, [cart])
 
   function addToCart(item) {
     const itemExists = cart.findIndex((guitar) => guitar.id === item.id)
